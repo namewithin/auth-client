8fd98e6937e3d17001cfb9428bdfb758c4e64496
Aleksey Kostenko <aleksey.kostenko.dev@gmail.com>
2022-05-17 17:14:10 +0300

    replace DiactorosFactory to PsrHttpFactory

--- src/Guards/ClientGuard.php
+++ src/Guards/ClientGuard.php
@@ -4,6 +4,7 @@ namespace Supervisor\Auth\Guards;
 
 use Exception;
 use Firebase\JWT\JWT;
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Laravel\Passport\Passport;
 use Illuminate\Auth\GuardHelpers;
 use Illuminate\Contracts\Auth\Authenticatable;
@@ -13,12 +14,17 @@ use Illuminate\Container\Container;
 use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Contracts\Encryption\Encrypter;
 use Illuminate\Http\Request;
+use Laminas\Diactoros\ResponseFactory;
+use Laminas\Diactoros\ServerRequestFactory;
+use Laminas\Diactoros\StreamFactory;
+use Laminas\Diactoros\UploadedFileFactory;
 use Laravel\Passport\Client;
 use Laravel\Passport\ClientRepository;
 use Laravel\Passport\TokenRepository;
 use League\OAuth2\Server\Exception\OAuthServerException;
 use League\OAuth2\Server\ResourceServer;
-use Symfony\Bridge\PsrHttpMessage\Factory\DiactorosFactory;
+use Psr\Http\Message\ServerRequestInterface;
+use Symfony\Bridge\PsrHttpMessage\Factory\PsrHttpFactory;
 
 class ClientGuard implements Guard
 {
@@ -27,55 +33,55 @@ class ClientGuard implements Guard
     /**
      * The request instance.
      *
-     * @var \Illuminate\Http\Request
+     * @var Request
      */
     protected $request;
     /**
      * The resource server instance.
      *
-     * @var \League\OAuth2\Server\ResourceServer
+     * @var ResourceServer
      */
     protected $server;
     /**
      * The user provider implementation.
      *
-     * @var \Illuminate\Contracts\Auth\UserProvider
+     * @var UserProvider
      */
     protected $provider;
     /**
      * The token repository instance.
      *
-     * @var \Laravel\Passport\TokenRepository
+     * @var TokenRepository
      */
     protected $tokens;
     /**
      * The client repository instance.
      *
-     * @var \Laravel\Passport\ClientRepository
+     * @var ClientRepository
      */
     protected $clients;
     /**
      * The encrypter implementation.
      *
-     * @var \Illuminate\Contracts\Encryption\Encrypter
+     * @var Encrypter
      */
     protected $encrypter;
     /**
      * The client repository instance.
      *
-     * @var \Laravel\Passport\ClientRepository
+     * @var ClientRepository
      */
     private $client;
 
     /**
      * Create a new token guard instance.
      *
-     * @param  \Illuminate\Http\Request $request
-     * @param  \League\OAuth2\Server\ResourceServer  $server
-     * @param  \Illuminate\Contracts\Auth\UserProvider  $provider
-     * @param  \Laravel\Passport\TokenRepository  $tokens
-     * @param  \Laravel\Passport\ClientRepository  $client
-     * @param  \Illuminate\Contracts\Encryption\Encrypter  $encrypter
+     * @param Request $request
+     * @param ResourceServer $server
+     * @param UserProvider $provider
+     * @param TokenRepository $tokens
+     * @param ClientRepository $client
+     * @param Encrypter $encrypter
      * @return void
      */
     public function __construct(
@@ -97,7 +103,8 @@ class ClientGuard implements Guard
     /**
      * Get the currently authenticated user.
      *
-     * @return \Illuminate\Contracts\Auth\Authenticatable|null
+     * @return Authenticatable|null
+     * @throws BindingResolutionException
      */
     public function user(): ?Authenticatable
     {
@@ -118,8 +125,9 @@ class ClientGuard implements Guard
     /**
      * Get the client for the incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
      * @return mixed
+     * @throws BindingResolutionException
      */
     public function oauthClient(Request $request): ?Client
     {
@@ -147,15 +155,21 @@ class ClientGuard implements Guard
     /**
      * Authenticate and get the incoming PSR-7 request via the Bearer token.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Psr\Http\Message\ServerRequestInterface
+     * @param Request $request
+     * @return ServerRequestInterface
+     * @throws BindingResolutionException
      */
     protected function getPsrRequestViaBearerToken($request)
     {
         // First, we will convert the Symfony request to a PSR-7 implementation which will
         // be compatible with the base OAuth2 library. The Symfony bridge can perform a
         // conversion for us to a Zend Diactoros implementation of the PSR-7 request.
-        $psr = (new DiactorosFactory)->createRequest($request);
+        $psr = (new PsrHttpFactory(
+            new ServerRequestFactory,
+            new StreamFactory,
+            new UploadedFileFactory,
+            new ResponseFactory
+        ))->createRequest($request);
 
         try {
             return $this->server->validateAuthenticatedRequest($psr);
@@ -171,7 +185,7 @@ class ClientGuard implements Guard
     /**
      * Get the token cookie via the incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
      * @return mixed
      */
     protected function getTokenViaCookie($request)
@@ -199,7 +213,7 @@ class ClientGuard implements Guard
     /**
      * Decode and decrypt the JWT token cookie.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
      * @return array
      */
     protected function decodeJwtTokenCookie($request)
@@ -215,7 +229,7 @@ class ClientGuard implements Guard
      * Determine if the CSRF / header are valid and match.
      *
      * @param  array  $token
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
      * @return bool
      */
     protected function validCsrf($token, $request)
